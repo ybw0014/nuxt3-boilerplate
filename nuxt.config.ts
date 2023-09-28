5efb7711aba1382d8a76a86ab82b1b424cbdd175
ybw0014 <i@guizhanss.com>
2023-09-28 10:57:53 -0400

    feat: add i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,33 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
+    "@nuxtjs/i18n",
     "@nuxtjs/eslint-module"
-  ]
+  ],
+  i18n: {
+    locales: [
+      {
+        code: "en",
+        iso: "en-US",
+        name: "English",
+        file: "en-US.json"
+      },
+      {
+        code: "zh-CN",
+        iso: "zh-CN",
+        name: "简体中文",
+        file: "zh-CN.json"
+      }
+    ],
+    lazy: true,
+    strategy: "no_prefix",
+    langDir: "lang",
+    defaultLocale: "en",
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "i18n_locale",
+      alwaysRedirect: true,
+      fallbackLocale: "en"
+    }
+  }
 });
