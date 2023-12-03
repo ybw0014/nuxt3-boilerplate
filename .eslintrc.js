26ad277de86ff98a1ebe1a51a05ef7e7b8934a27
ybw0014 <i@guizhanss.com>
2023-12-02 22:46:50 -0500

    chore(eslint): use universal config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true
-  },
-  parser: "vue-eslint-parser",
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "@typescript-eslint/parser"
+    parser: '@typescript-eslint/parser'
   },
   extends: [
-    "@nuxtjs/eslint-config-typescript",
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "prettier"
+    '@guizhan/eslint-config',
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:vue/vue3-recommended',
+    'prettier'
   ],
   rules: {
-    indent: ["error", 2, { SwitchCase: 1 }],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-    "no-undef": ["off"],
-    "no-console": ["off"],
-    "require-await": ["off"],
-    "vue/multi-word-component-names": ["off"],
-    "vue/no-multiple-template-root": ["off"],
-    "@typescript-eslint/no-explicit-any": ["off"],
-    "@typescript-eslint/no-unused-vars": ["warn"],
-    "import/no-unresolved": ["off"]
+    'no-undef': ['off'],
+    'no-console': ['off'],
+    'vue/multi-word-component-names': ['off'],
+    'vue/no-multiple-template-root': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'import/no-unresolved': ['off']
   }
 };
